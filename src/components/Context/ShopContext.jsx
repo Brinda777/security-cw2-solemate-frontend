3de7f3d924aef6f94bf1a8f2d7645ab045d260c7
Brinda Bhattarai <brindaabrindaa7@gmail.com>
2025-01-22 14:21:05 +0545

    removed unused function

--- src/components/Context/ShopContext.jsx
+++ src/components/Context/ShopContext.jsx
@@ -6,14 +6,6 @@ const ShopContextProvider = (props) => {
 
   const [products, setProducts] = useState([]);
 
-  const getDefaultCart = () => {
-    let cart = {};
-    for (let i = 0; i < 300; i++) {
-      cart[i] = 0;
-    }
-    return cart;
-  };
-
   useEffect(() => {
     fetch(`${process.env['REACT_APP_BACKEND_URL']}/product/get-all`)
       .then((res) => res.json())
